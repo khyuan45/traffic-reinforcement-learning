4c0edafd550f89ffd40f43cad9001b24e6d49635
khyuan45 <36764123+khyuan45@users.noreply.github.com>
2019-04-17 16:33:11 +0800

    Update parameters.py

--- new_package/parameters.py
+++ new_package/parameters.py
@@ -1,4 +1,20 @@
 import numpy as np
+import pandas as pd
+
+Data_File_Path='E:\\project\\car\\201904\\dataoutput' #请在这里输入存放day1/2/19/25四个文件夹的路径
+ChosenDate='day1' #请在day1/2/19/25中选择
+'''
+其中这四天的情况分别为
+day1:晴天+工作日
+day2:雨天+工作日
+day19:雨天+周末
+day25:晴天+周末
+'''
+
+#以下四个是四个路口的转移矩阵，使用dayx_Cross1\2\3\4.csv
+transition_matrix_data=[]
+for i in range(1,5):
+    transition_matrix_data.append(np.array(pd.read_csv(Data_File_Path+'\\'+ChosenDate+'\\'+ChosenDate+'_Cross'+str(i)+'.csv',index_col=0)))
 
 
 def speed_matrix(id, epoch):
@@ -16,8 +32,8 @@ def speed_matrix(id, epoch):
 
 
 def transition_matrix(id, epoch):
-
-    return
+    cross=ord(id)-65 #ABCD通过ASCII读取转为0123
+    return transition_matrix_data[cross]
 
 
 def poisson_matrix(id, epoch):
